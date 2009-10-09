fc1914a1d86dc0ec05c5debe02f4c658411ab0cb
unknown <Phil@.(none)>
2009-10-09 20:33:15 +0800

    Initial Commit

---
+++ src/UnitTests/Test1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace Tao.UnitTests
+{
+	[TestFixture]
+	public class Test1
+	{
+		[Test]
+		public void ThisIsAnExampleTest()
+		{
+			Assert.AreEqual(2, 1+1);
+		}
+	}
+}
+
