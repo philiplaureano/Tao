6ca39aa3f350061f5a52993b24d2e0f784c200e6
Philip Laureano <philip.laureano@gmail.com>
2010-04-06 11:53:29 +0800

    Added support for getting the metadata stream count and seeking the metadata root position; also fixed Nant buildfile errors

---
+++ src/Core/ReadMetadataStreamCount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Tao.Interfaces;
+
+namespace Tao.Core
+{
+    /// <summary>
+    /// Represents a type that determines the number of metadata streams within a given Portable Executable stream.
+    /// </summary>
+    public class ReadMetadataStreamCount : IFunction<Stream, int>
+    {
+        private readonly IFunction<Stream> _seekMetadataRootPosition;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public ReadMetadataStreamCount(IFunction<Stream> seekMetadataRootPosition)
+        {
+            _seekMetadataRootPosition = seekMetadataRootPosition;
+        }
+
+        /// <summary>
+        /// Determines the number of metadata streams within a given Portable Executable stream.
+        /// </summary>
+        /// <param name="input">The input stream.</param>
+        /// <returns>The number of metadata streams in the current PE stream.</returns>
+        public int Execute(Stream input)
+        {
+            _seekMetadataRootPosition.Execute(input);
+            input.Seek(0x1e, SeekOrigin.Current);
+
+            var reader = new BinaryReader(input);
+            return reader.ReadInt16();
+        }
+    }
+}
--- src/UnitTests/MetadataRootTests.cs
+++ src/UnitTests/MetadataRootTests.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using Tao.Interfaces;
+using Tao.Containers;
 
 namespace Tao.UnitTests
 {
     [TestFixture]
-    public class MetadataRootTests : BaseStreamTests 
-    {        
+    public class MetadataRootTests : BaseStreamTests
+    {
+        [Test]
+        public void ShouldBeAbleToSeekMetadataRootPosition()
+        {
+            var stream = GetStream();
+            var container = CreateContainer();
+
+            var seeker = container.GetInstance<IFunction<Stream>>("SeekMetadataRootPosition");
+            Assert.IsNotNull(seeker);
+
+            seeker.Execute(stream);
+            Assert.AreEqual(0x260, stream.Position);
+        }
+
+        [Test]
+        public void ShouldBeAbleToReadMetadataStreamCount()
+        {
+            var stream = GetStream();
+            var container = CreateContainer();
+
+            var reader = container.GetInstance<IFunction<Stream, int>>("ReadMetadataStreamCount");
+            Assert.IsNotNull(reader);
+
+            var count = reader.Execute(stream);
+            Assert.AreEqual(5, count);
+        }
     }
 }
