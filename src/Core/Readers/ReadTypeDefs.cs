7a01b6e3262a102233cbd38c434ca919cb791e20
Philip Laureano <philip.laureano@gmail.com>
2010-04-17 07:16:00 +0800

    Added support for reading the extends column from the typedef table

--- src/Core/Readers/ReadTypeDefs.cs
+++ src/Core/Readers/ReadTypeDefs.cs
@@ -17,8 +17,16 @@ namespace Tao.Readers
         private readonly IFunction<ITuple<uint, Stream>, string> _readStringFromStringHeap;
         private readonly IFunction<ITuple<TableId, Stream>, int> _getTableRowSize;
         private readonly IFunction<ITuple<int, BinaryReader>, uint> _readHeapIndexValue;
+        private readonly IFunction<ITuple<CodedTokenType, int>, ITuple<TableId, int>> _getTableReferenceFromCodedToken;
 
-        public ReadTypeDefs(IFunction<Stream, IDictionary<TableId, ITuple<int, Stream>>> readAllMetadataTables, IFunction<Stream, ITuple<int, int, int>> readMetadataHeapIndexSizes, IFunction<ITuple<uint, Stream>, string> readStringFromStringHeap, IFunction<ITuple<TableId, Stream>, int> getTableRowSize, IFunction<ITuple<int, BinaryReader>, uint> readHeapIndexValue)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadTypeDefs"/> class.
+        /// </summary>
+        public ReadTypeDefs(IFunction<Stream, IDictionary<TableId, ITuple<int, Stream>>> readAllMetadataTables, 
+            IFunction<Stream, ITuple<int, int, int>> readMetadataHeapIndexSizes, 
+            IFunction<ITuple<uint, Stream>, string> readStringFromStringHeap, 
+            IFunction<ITuple<TableId, Stream>, int> getTableRowSize, 
+            IFunction<ITuple<int, BinaryReader>, uint> readHeapIndexValue)
         {
             _readAllMetadataTables = readAllMetadataTables;
             _readMetadataHeapIndexSizes = readMetadataHeapIndexSizes;
@@ -51,14 +59,54 @@ namespace Tao.Readers
                 tableStream.Seek(offset, SeekOrigin.Begin);
                 var typeDef = new TypeDef();
 
-                typeDef.Flags = (TypeAttributes) reader.ReadUInt32();
-                var nameIndex = _readHeapIndexValue.Execute(stringHeapIndexSize, reader);
-                var namespaceIndex = _readHeapIndexValue.Execute(stringHeapIndexSize, reader);
+                Read(stringHeapIndexSize, typeDef, input, reader, tables, rowSize, i);
 
-                typeDef.Name = _readStringFromStringHeap.Execute(nameIndex, input);
-                typeDef.Namespace = _readStringFromStringHeap.Execute(namespaceIndex, input);
                 yield return typeDef;
             }
+        }
+
+        private void Read(int stringHeapIndexSize, TypeDef typeDef, Stream input, BinaryReader reader, IDictionary<TableId, ITuple<int, Stream>> tables, int rowSize, int currentRow)
+        {
+            var typeDefTable = tables[TableId.TypeDef];
+            var rowCount = typeDefTable.Item1;
+
+            typeDef.Flags = (TypeAttributes) reader.ReadUInt32();
+            var nameIndex = _readHeapIndexValue.Execute(stringHeapIndexSize, reader);
+            var namespaceIndex = _readHeapIndexValue.Execute(stringHeapIndexSize, reader);
+
+            typeDef.Name = _readStringFromStringHeap.Execute(nameIndex, input);
+            typeDef.Namespace = _readStringFromStringHeap.Execute(namespaceIndex, input);
+
+            var tableIds = new TableId[] {TableId.TypeDef, TableId.TypeRef, TableId.TypeSpec};
+            int token = ReadToken(tables, reader, tableIds);
+
+            if (token == 0)
+                return;
+
+            var extends = _getTableReferenceFromCodedToken.Execute(CodedTokenType.TypeDefOrTypeRef, token);
+            if (rowCount <= 1 || currentRow >= rowCount)
+                return;
+
+        }
+
+        private int ReadToken(IDictionary<TableId, ITuple<int, Stream>> tables, BinaryReader reader, IEnumerable<TableId> tableIds)
+        {
+            var shouldUseDwordIndices = false;
+            foreach(var tableId in tableIds)
+            {
+                if (!tables.ContainsKey(tableId))
+                    continue;
+
+                var currentTable = tables[tableId];
+                var currentRowCount = currentTable.Item1;
+                if (currentRowCount < ushort.MaxValue)
+                    continue;
+
+                shouldUseDwordIndices = true;
+                break;
+            }
+
+            return shouldUseDwordIndices ? reader.ReadInt32() : reader.ReadInt16();
         }
     }
 }
