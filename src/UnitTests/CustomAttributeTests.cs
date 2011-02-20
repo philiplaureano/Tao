751cf47c0d9990340cba6ecb2fc9ac10e3ec7823
Philip Laureano <philip.laureano@gmail.com>
2011-02-20 13:02:35 +0800

    Added SerString read support for custom attribute elements

--- src/UnitTests/CustomAttributeTests.cs
+++ src/UnitTests/CustomAttributeTests.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using Tao.Interfaces;
+using Tao.Containers;
 
 namespace Tao.UnitTests
 {
@@ -22,6 +24,25 @@ namespace Tao.UnitTests
         public void ShouldBeAbleToExtractAttributeSignatureFromMethodSignature()
         {
             throw new NotImplementedException();
+        }
+
+        [Test]
+        public void ShouldBeAbleToReadSerString()
+        {
+            var text = "Named1";
+            byte length = (byte)text.Length;
+            var textBytes = Encoding.UTF8.GetBytes(text);
+
+            var bytes = new List<byte>();
+            bytes.Add(length);
+            bytes.AddRange(textBytes);            
+
+            var reader = container.GetInstance<IFunction<Stream, string>>("ReadSerString");
+            Assert.IsNotNull(reader, "ReadSerString reader not found");
+
+            var stream = new MemoryStream(bytes.ToArray());
+            var result = reader.Execute(stream);
+            Assert.AreEqual(text, result);
         }
     }
 }
