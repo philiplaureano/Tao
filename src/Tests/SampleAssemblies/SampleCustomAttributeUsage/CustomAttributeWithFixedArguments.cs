d29efc3800493a1b96b85a3b462825be7f89f51c
unknown <Phil@.(none)>
2012-03-29 20:25:39 +1100

    Added test stubs for reading custom attributes

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/CustomAttributeWithFixedArguments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomAttributeWithFixedArgumentsAttribute : Attribute
+    {
+        private int _value;
+
+        public CustomAttributeWithFixedArgumentsAttribute(int value)
+        {
+            _value = value;
+        }
+
+        public int Value
+        {
+            get { return _value; }
+        }
+    }
+}
