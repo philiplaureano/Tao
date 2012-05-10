2178185659ece8be41adec832f3946a9ff8c9832
Philip Laurean <philip.laureano@gmail.com>
2012-05-10 16:10:25 +1000

    Added stub code for reading the CustomAttribute signatures

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SampleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [CustomAttributeWithFixedArguments(42)]
+    public class SampleClass
+    {
+    }
+}
