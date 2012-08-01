96d309bdf4a2f1d5dfbc68d0b4f9eb052e3acaa4
Philip Laureano <philip.laureano@gmail.com>
2012-08-01 18:37:29 +1000

    Added missing files

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/CustomAttributeWithUInt32ArgumentsAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomAttributeWithUInt32ArgumentsAttribute : Attribute
+    {
+        private uint _value;
+
+        public CustomAttributeWithUInt32ArgumentsAttribute(uint value)
+        {
+            _value = value;
+        }
+
+        public uint Value
+        {
+            get { return _value; }
+        }
+    }
+}
