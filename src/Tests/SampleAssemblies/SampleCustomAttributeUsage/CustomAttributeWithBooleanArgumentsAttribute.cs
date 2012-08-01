0ce4e79eb001976007cfb713a1a698aa2560ba28
Philip Laureano <philip.laureano@gmail.com>
2012-08-01 18:17:05 +1000

    Added support for reading boolean arguments from a custom attribute instantiation

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/CustomAttributeWithBooleanArgumentsAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomAttributeWithBooleanArgumentsAttribute : Attribute
+    {
+        private bool _value;
+
+        public CustomAttributeWithBooleanArgumentsAttribute(bool value)
+        {
+            _value = value;
+        }
+
+        public bool Value
+        {
+            get { return _value; }
+        }
+    }
+}
---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SampleClassWithBooleanAttributeUsage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [CustomAttributeWithBooleanArguments(true)]
+    public class SampleClassWithBooleanAttributeUsage
+    {
+    }
+}
