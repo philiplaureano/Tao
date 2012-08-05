da73dcca98a7c9374ccb20f508d4a052ee63d4d7
Philip Laureano <philip.laureano@gmail.com>
2012-08-05 22:34:37 +1000

    Added initial test implementation for tracking down a bug in the MethodDefSignature reader that causes it to read an incorrect row value for a value type parameter

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/CustomAttributeWithSampleEnumArgumentsAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomAttributeWithSampleEnumArgumentsAttribute : Attribute
+    {
+        private SampleEnum _value;
+
+        public CustomAttributeWithSampleEnumArgumentsAttribute(SampleEnum value)
+        {
+            _value = value;
+        }
+
+        public SampleEnum Value
+        {
+            get { return _value; }
+        }
+    }
+}
---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SampleClassWithSampleEnumAttributeUsage.cs
@@ -0,0 +1,7 @@
+namespace SampleCustomAttributeUsage
+{
+    [CustomAttributeWithSampleEnumArguments(SampleEnum.Maybe)]
+    public class SampleClassWithSampleEnumAttributeUsage
+    {
+    }
+}
