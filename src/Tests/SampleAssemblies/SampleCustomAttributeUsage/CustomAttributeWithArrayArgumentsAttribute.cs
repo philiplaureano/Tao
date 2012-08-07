f4fc05ae76fa1860fea84dd21c1de8d05d538af8
Philip Laureano <philip.laureano@gmail.com>
2012-08-07 16:47:09 +1000

    Added stub test for reading array arguments declared in custom attribute signatures

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/CustomAttributeWithArrayArgumentsAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomAttributeWithArrayArgumentsAttribute : Attribute
+    {
+        private object[] _value;
+
+        public CustomAttributeWithArrayArgumentsAttribute(object[] value)
+        {
+            _value = value;
+        }
+
+        public object[] Value
+        {
+            get { return _value; }
+        }
+    }
+}
---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SampleClassWithArrayAttributeUsage.cs
@@ -0,0 +1,7 @@
+namespace SampleCustomAttributeUsage
+{
+    [CustomAttributeWithArrayArguments(new object[]{1,2,3})]
+    public class SampleClassWithArrayAttributeUsage
+    {
+    }
+}
