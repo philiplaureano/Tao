fd48ccfafbc807130519483bff7200c38e09303f
Philip Laureano <philip.laureano@gmail.com>
2012-08-08 16:49:49 +1000

    Added support for reading boxed arrays in custom attribute signatures

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SampleClassWithBoxedArrayArgumentAttributeUsage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [CustomAttributeWithObjectArguments(new []{1, 2, 3, 4, 5, 6})]
+    public class SampleClassWithBoxedArrayArgumentAttributeUsage
+    {
+    }
+}
