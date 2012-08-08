a78d593c80a90a4538baf53eec080c33c77bd04f
Philip Laureano <philip.laureano@gmail.com>
2012-08-09 06:59:43 +1000

    Added test code for named fields in custom attribute signatures

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/CustomAttributeWithNamedFieldAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomAttributeWithNamedFieldAttribute : Attribute
+    {
+        public int SomeField;
+    }
+}
---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SampleClassWithNamedFieldAttributeUsage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [CustomAttributeWithNamedField(SomeField = 12345)]
+    public class SampleClassWithNamedFieldAttributeUsage
+    {
+    }
+}
