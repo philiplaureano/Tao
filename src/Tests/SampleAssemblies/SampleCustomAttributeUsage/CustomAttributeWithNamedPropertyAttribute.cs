2ffc221e308a45bcba9a4cac94fefcc788dfb16f
Philip Laureano <philip.laureano@gmail.com>
2012-08-10 13:36:38 +1000

    Finished adding support for reading named properties from custom attribute signatures

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/CustomAttributeWithNamedPropertyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CustomAttributeWithNamedPropertyAttribute : Attribute
+    {
+        public int SomeProperty { get; set; }
+    }
+}
