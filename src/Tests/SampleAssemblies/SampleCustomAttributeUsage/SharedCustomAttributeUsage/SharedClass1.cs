28a9ab0836bd2adcd5612f0bb13e841ea82cda06
Philip Laureano <philip.laureano@gmail.com>
2012-08-22 08:05:50 +1000

    Added support for retrieving the list of parent rows associated with the particular usage of a custom attribute constructor signature

---
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SharedCustomAttributeUsage/SharedClass1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCustomAttributeUsage.SharedCustomAttributeUsage
+{
+    [Foo]
+    public class SharedClass1
+    {
+    }
+}
