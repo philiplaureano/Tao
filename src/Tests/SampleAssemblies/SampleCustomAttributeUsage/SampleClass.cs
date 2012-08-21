2a55d142d72300ec9a98cb108527b7533b950fba
Philip Laureano <philip.laureano@gmail.com>
2012-08-21 16:08:36 +1000

    Added support for getting custom attribute rows for any type that implements IHasCustomAttribute

--- src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SampleClass.cs
+++ src/Tests/SampleAssemblies/SampleCustomAttributeUsage/SampleClass.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace SampleCustomAttributeUsage
 {
-    [AttributeUsage(AttributeTargets.Class)]
-    public class FooAttribute : Attribute
-    {
-    }
-
     [CustomAttributeWithFixedArguments(42)]
     [Foo]
     public class SampleClass
