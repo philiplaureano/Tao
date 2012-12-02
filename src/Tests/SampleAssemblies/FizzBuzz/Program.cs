ed7593df86eff74c78b166accf4d04cf4fd84db2
Philip Laureano <philip.laureano@gmail.com>
2012-12-02 15:27:01 +1100

    Added support for precalculating the instruction offsets using a given list of instructions

---
+++ src/Tests/SampleAssemblies/FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                if (i % 3 != 0 || i % 5 != 0)
+                {
+                    if (i % 3 == 0)
+                        Console.WriteLine("Fizz");
+
+                    if (i % 5 == 0)
+                        Console.WriteLine("Buzz");
+
+                    continue;
+                }
+
+                Console.WriteLine("FizzBuzz");
+            }
+        }
+    }
+}
